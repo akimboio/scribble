5f3f6fb61b1e163126c33a8abb82f3c1515737a4
Josh Marlow <josh.marlow@retickr.com>
2011-12-14 15:45:19 -0500

    * Changed some tabs (eww) to spaces

--- scribble_config.py
+++ scribble_config.py
@@ -17,6 +17,6 @@ class server:
     maxConnectionBacklog = socket.SOMAXCONN
 
 class cassandra:
-	keyspace = "retickr_logs"
-	hosts = ["192.168.117.64","192.168.117.61","192.168.117.62","192.168.117.63"]
-	server_port=9160
+    keyspace = "retickr_logs"
+    hosts = ["192.168.117.64","192.168.117.61","192.168.117.62","192.168.117.63"]
+    server_port=9160
